84e42bf5dffb8adc4ce83af035d50e0a10be611d
nhatvu148 <nhatvu148@gmail.com>
2024-05-09 22:05:18 +0700

    use apply_async

--- celery/app.py
+++ celery/app.py
@@ -11,9 +11,11 @@ def print_result(task_id, task_name):
     print(result.status)
     print(result.result)
 
-res_add = add.delay(123, 456)
+# res_add = add.delay(123, 456)
+res_add = add.apply_async((123, 456))
 add_task_id= res_add.task_id
 print_result(res_add.task_id, 'add')
 
-res_reverse = reverse.delay('Hello World')
+# res_reverse = reverse.delay('Hello World')
+res_reverse = reverse.apply_async(args=['Hello World'])
 print_result(res_reverse.task_id, 'reverse')
